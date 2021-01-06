d6b9f446f524def6f25cad7c0505828c73aa699f
ththo <tho@deltabrains.vn>
2021-01-06 16:13:45 +0700

    update backup function

--- amplify/backend/function/smartorderBackupDynamoDB/src/index.js
+++ amplify/backend/function/smartorderBackupDynamoDB/src/index.js
@@ -2,25 +2,23 @@ var aws = require('aws-sdk');
 const dynamodb = new aws.DynamoDB();
 
 exports.handler = (event, context, callback) => {
-    console.log('Received event: ', event);
-    var tablesToBackup = event.tablesToBackup.split(",");
-    var promises = tablesToBackup.map(backupTable);
+  var tablesToBackup = event.tablesToBackup.split(",");
+  var promises = tablesToBackup.map(backupTable);
 
-    Promise.all(promises)
-      .then(result => { console.log(result); callback(); })
-      .catch(reason => { console.log(reason); callback(reason); });
+  Promise.all(promises)
+      .then(result => { callback(); })
+      .catch(reason => { callback(reason); });
 };
 
 function backupTable(tablename) {
   var timestamp = new Date().toISOString()
-    .replace(/\..+/, '')
-    .replace(/:/g, '')
-    .replace(/-/g, '');
+      .replace(/\..+/, '')
+      .replace(/:/g, '')
+      .replace(/-/g, '');
 
   var params = {
     TableName: tablename,
     BackupName: tablename + '-' + timestamp
   };
-  console.log(params);
   return dynamodb.createBackup(params).promise();
 }
